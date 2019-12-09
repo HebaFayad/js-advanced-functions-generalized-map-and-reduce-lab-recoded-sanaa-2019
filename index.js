f45f8c7aff16f366bea4576f9b6127d4fef77a52
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-12-09 06:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const map = function(arr,fn){
   return arr.map(fn);
 }
-const reduce = function(arr,callback,startingPoint){
-  if(startingPoint != null){
+const reduce = function(arr,callback,start){
+  if(start != null){
     return arr.reduce(callback,startingPoint);
   }
   return arr.reduce(callback);
